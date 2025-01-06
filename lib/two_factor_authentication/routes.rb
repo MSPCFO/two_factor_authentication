222dc872cb4f5320f8fa39defa15c4749312de74
Kevin Pheasey <kevin.pheasey@gmail.com>
2025-01-06 16:20:15 -0500

    fix(routes): remove non-standard action resend_code from the `only` option
    Fixes Rails 8 support for enforced stricter constraints on the allowed controller actions

--- lib/two_factor_authentication/routes.rb
+++ lib/two_factor_authentication/routes.rb
@@ -3,8 +3,13 @@ module ActionDispatch::Routing
     protected
 
       def devise_two_factor_authentication(mapping, controllers)
-        resource :two_factor_authentication, :only => [:show, :update, :resend_code], :path => mapping.path_names[:two_factor_authentication], :controller => controllers[:two_factor_authentication] do
-          collection { get "resend_code" }
+        resource :two_factor_authentication,
+                 only: [:show, :update],
+                 path: mapping.path_names[:two_factor_authentication],
+                 controller: controllers[:two_factor_authentication] do
+          collection do
+            get :resend_code
+          end
         end
       end
   end
